d904d13f02f6cdd92ffb44ec9a6e26f93629abc7
Vadym <vadymkravtcsov@gmail.com>
2025-12-29 12:20:09 +0200

    add linkedList create linkedList

---
+++ linkedList/linkedlist_learning.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.size += 1
+
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.size += 1
+
+
+    def remove_first(self):
+        if self.head is None:
+            return None
+
+        data = self.head.data
+        self.head = self.head.next
+
+        if self.head is None:
+            self.tail = None
+
+        self.size -= 1
+        return data
+
+
+        # Create function, remove last data
